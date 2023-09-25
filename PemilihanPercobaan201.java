20a517ad93336e157e64b16d50112a2ee4537094
abdullatifm2711 <abdul.latifm2711@gmail.com>
2023-09-25 18:55:35 +0700

    Praktikum5

--- PemilihanPercobaan201.java
+++ PemilihanPercobaan201.java
@@ -13,8 +13,30 @@ public class PemilihanPercobaan201 {
         float tugas = input01.nextFloat();
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
-        String message = total <65 ? "Remidi" : "Tidak remidi";
-        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+        //String message = total <65 ? "Remidi" : "Tidak remidi";
+       // System.out.println("Nilai akhir = " + total + " sehingga " + message);
+       
+       if (80 < total & total <= 100){
+           System.out.println("Nilai A (Sangat Baik)");
+       }
+        else if (73 < total & total  <= 80){
+            System.out.println("Nilai B+ (Lebih dari Baik)");
+        }
+        else if (65 < total & total <= 73){
+            System.out.println("Nilai B (Baik)");
+        }
+        else if (60 < total & total <= 65){
+            System.out.println("Nilai C+ (Lebih dari Cukup)");
+        }
+        else if (50 < total & total <= 60){
+            System.out.println("Nilai C (Cukup)");
+        }
+        else if (34 < total & total <= 50){
+            System.out.println("Nilai D (Kurang)");
+        }
+        else {
+            System.out.println("Nilai E (gagal)");
+        }
     }
 
 }
