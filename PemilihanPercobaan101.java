c064836c60877f7862a70ea663dbdd8831c72ebe
abdullatifm2711 <abdul.latifm2711@gmail.com>
2023-09-25 17:57:48 +0700

    Praktikum5

--- PemilihanPercobaan101.java
+++ PemilihanPercobaan101.java
@@ -6,10 +6,14 @@ public class PemilihanPercobaan101{
        Scanner input01 = new Scanner(System.in);
        System.out.print("Masukkan angka; ");
        int angka = input01.nextInt();
-       if (angka  % 2 == 0)
-       System.out.println("Angka "+angka+"bilangan genap");
-       else
-       System.out.println("Angka "+angka+"bilangan ganjil");
+
+      // if (angka  % 2 == 0)
+        //System.out.println("Angka "+angka+"bilangan genap");
+       //else{
+        //System.out.println("Angka "+angka+"bilangan ganjil");
+
+        String jawaban = (angka %2==0)? ("Angka" + angka + "bilangan genap") : ("Angka" + angka + "bilangan ganjil");
+        System.out.println(angka + "adalah"  + jawaban);
 
     }
 
